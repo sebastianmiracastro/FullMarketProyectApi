bb65b1c7b7263dd41199c4682abf830471711be2
Edwin Rozo <erozog125@gmail.com>
2021-12-01 14:47:16 -0500

    Se corrigen errores en main y se encuentra actualizado. 01012021

--- src/Components/Page/HomePageLogin/HomePageLogin.jsx
+++ src/Components/Page/HomePageLogin/HomePageLogin.jsx
@@ -1,9 +1,4 @@
-<<<<<<< HEAD:src/Components/Page/LoginViewPage/HomeLoginPage.jsx
-import './HomeLoginPage.css'
-import React from 'react'
-=======
 import './HomePageLogin.css'
->>>>>>> cambios_ed:src/Components/Page/HomePageLogin/HomePageLogin.jsx
 import { LoginPage } from '../../Layout/LoginViewLayout/LayoutLoginView'
 
 export const HomePageLogin = () => {
--- src/Components/UI/MainViewUI/HeaderMainPage/Header.jsx
+++ src/Components/UI/MainViewUI/HeaderMainPage/Header.jsx
@@ -1,11 +1,7 @@
 import './Header.css'
 import ImgLogoFullMarket from '../../../../Images/Images-MainView/FullMarket.jpeg'
-<<<<<<< HEAD
-import { Link } from 'react-router-dom'
-=======
 import { Link } from 'react-router-dom';
 
->>>>>>> cambios_ed
 
 export const Header =()=>{
     return(
@@ -14,15 +10,8 @@ export const Header =()=>{
                 <img src={ImgLogoFullMarket} alt="" className="ImgContent"></img>
                 <nav className="NavBarContainer">
                     <div className="ContentOptionsNavBar">
-<<<<<<< HEAD
-                        <Link className="OptionNavbar" value="Iniciar_sesion" to="/Login">
-                            <button className="ButtonLoginInitial">Iniciar Sesion</button>
-                        </Link>
-                        <a className="OptionNavbar" value="Registrarse">Registrarse</a>
-=======
                         <Link to="/"><a className="OptionNavbar" value="Iniciar_sesion">Home</a></Link>
                         <Link to="/Login"><a className="OptionNavbar" value="Iniciar_sesion">Iniciar sesion</a></Link>                        
->>>>>>> cambios_ed
                     </div>
                 </nav>                    
             </div>
