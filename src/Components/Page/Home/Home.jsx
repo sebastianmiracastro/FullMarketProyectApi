6d7a6b5984ab759bc2b389846b15634e5c807b6a
Edwin Rozo <erozog125@gmail.com>
2021-12-01 14:42:37 -0500

    Resolviendo conflictos en main

--- src/Components/Page/Home/Home.jsx
+++ src/Components/Page/Home/Home.jsx
@@ -1,9 +1,4 @@
-<<<<<<< HEAD:src/Components/Page/MainViewPage/MainPage.jsx
-import './MainPage.css'
-import React from 'react'
-=======
 import './Home.css'
->>>>>>> cambios_ed:src/Components/Page/Home/Home.jsx
 import { PageContent } from '../../Layout/MainViewLayout/LayoutMainView'
 
 export const Home = () =>{
